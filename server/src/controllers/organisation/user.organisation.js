377412cfa6ca1537eddb032a623127669bb3270e
amandeep25 <st.amandeep25@gmail.com>
2026-06-22 10:30:00 +0530

    dashboard stats and team performance controller done

--- server/src/controllers/organisation/user.organisation.js
+++ server/src/controllers/organisation/user.organisation.js
@@ -2,6 +2,8 @@ import { User } from "../../models/user.models.js";
 import { ApiError } from "../../utils/ApiError.js";
 import { asyncHandler } from "../../utils/AsyncHandler.js"
 import { ApiResponse } from "../../utils/ApiResponse.js"
+import { Project } from "../../models/project.models.js";
+import { Task } from "../../models/task.models.js";
 
 const createNewUser = asyncHandler(async(req,res)=>{
 
@@ -56,7 +58,118 @@ const getAllUser = asyncHandler(async(req,res)=>{
     )
 })
 
+const getDashboardStats = asyncHandler(async(req,res)=>{
+    //total users
+    //Active projects
+    //completed tasks
+    //pending tasks
+
+    const totalUsers = await User.countDocuments({
+        organisation:req.user?.organisation,
+        role:{ $ne:"admin"}
+    })
+
+    const activeProjects = await Project.countDocuments({
+        organisation:req.user?.organisation,
+        status:"active"
+    })
+
+    const completedTasks = await Task.countDocuments({
+        organisation:req.user?.organisation,
+        status:"done"
+    })
+
+    const pendingTasks = await Task.countDocuments({
+        organisation:req.user?.organisation,
+        status:{$in :["todo","in-progress"]}
+    })
+
+
+
+    res.status(200)
+    .json(
+        new ApiResponse(200,{
+            totalUsers,
+            activeProjects,
+            completedTasks,
+            pendingTasks
+        },"Fetched")
+    )
+})
+
+const getDashboardTeamPerformance = asyncHandler(async(req,res)=>{
+    const userStats = await User.aggregate([
+    {
+        $match: {
+            organisation: req.user.organisation,
+            role: { $ne: "admin" }
+        }
+    },
+    {
+        $group: {
+            _id: { $month: "$createdAt" },
+            users: { $sum: 1 }
+        }
+    },
+    {
+        $sort: {
+            "_id": 1
+        }
+    }
+    ]);
+
+    const taskStats = await Task.aggregate([
+    {
+        $match: {
+            organization: req.user.organisation
+        }
+    },
+    {
+        $group: {
+            _id: { $month: "$createdAt" },
+            tasks: { $sum: 1 }
+        }
+    },
+    {
+        $sort: {
+            "_id": 1
+        }
+    }
+    ]);
+
+    const months = [
+        "Jan","Feb","Mar","Apr",
+        "May","Jun","Jul","Aug",
+        "Sep","Oct","Nov","Dec"
+    ];
+
+    const chartData = months.map((month, index) => {
+        const task = taskStats.find(
+            item => item._id === index + 1
+        );
+
+        const user = userStats.find(
+            item => item._id === index + 1
+        );
+
+        return {
+            month,
+            tasks: task?.tasks || 0,
+            users: user?.users || 0
+        };
+    });
+
+
+    res
+    .status(200)
+    .json(
+        new ApiResponse(200,chartData,"Fetched")
+    )
+})
+
 export {
     createNewUser,
     getAllUser,
+    getDashboardStats,
+    getDashboardTeamPerformance,
 }
--- server/src/routes/organisation.routes.js
+++ server/src/routes/organisation.routes.js
@@ -1,11 +1,13 @@
 import {Router} from "express";
 import { verifyUser } from "../middlewares/verifyJWT.js"
 import { isAdmin } from "../middlewares/isAdmin.js"
-import { createNewUser, getAllUser } from "../controllers/organisation/user.organisation.js";
+import { createNewUser, getAllUser, getDashboardStats, getDashboardTeamPerformance } from "../controllers/organisation/user.organisation.js";
 
 const router = Router();
 
 router.route('/user').post(verifyUser,isAdmin,createNewUser)
 router.route('/users').get(verifyUser,isAdmin,getAllUser)
+router.route('/dashboard/stats').get(verifyUser,isAdmin,getDashboardStats)
+router.route('/dashboard/team-performance').get(verifyUser,isAdmin,getDashboardTeamPerformance)
 
 export default router;
