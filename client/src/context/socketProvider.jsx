eb2f452e9c698e8f76c4579bd5af4df1e816bf53
amandeep25 <st.amandeep25@gmail.com>
2026-06-24 22:23:05 +0530

    Some correction in previous written code

--- client/src/context/socketProvider.jsx
+++ client/src/context/socketProvider.jsx
@@ -1,19 +1,25 @@
 import { useEffect, useState } from "react"
 import { io } from "socket.io-client"
 import { SocketContext } from "./socketContext";
+import { useSelector } from "react-redux"
 
 const SocketProvider = ({children}) =>{
     const [socket,setSocket] = useState(null);
 
+    const { isloggedIn, user } = useSelector(state=>state.auth)
+
     useEffect(()=>{
-        const socketInstance = io("http:localhost:8080");
+
+        if(!(isloggedIn && user)) return;
+
+        const socketInstance = io("http://localhost:8080");
 
         setSocket(socketInstance);
 
         return () => {
             socketInstance.disconnect();
         }
-    },[])
+    },[isloggedIn,user])
 
     return (
         <SocketContext value={socket}>
--- server/socket.js
+++ server/socket.js
@@ -12,7 +12,7 @@ const io = new Server(server,{
 });
 
 io.on("connection",(socket)=>{
-    console.log(socket)
+    console.log(socket.id)
 })
 
 export {
