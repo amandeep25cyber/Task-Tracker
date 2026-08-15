00621caf2f43409cf3e69c2ca66923df4182a1dd
amandeep25 <st.amandeep25@gmail.com>
2026-08-15 23:46:10 +0530

    API creation for fetching the user's tasks

---
+++ server/src/controllers/member/Taskboard.member.js
@@ -0,0 +1,31 @@
+import { Task } from "../../models/task.models.js";
+import { ApiError } from "../../utils/ApiError.js";
+import { ApiResponse } from "../../utils/ApiResponse.js";
+import { asyncHandler } from "../../utils/AsyncHandler.js";
+
+const getUserTasks = asyncHandler(async(req,res)=>{
+    const userId = req.user?._id;
+
+    if(!userId){
+        throw new ApiError(403,"User not authorized or missing permissions");
+    }
+
+    const tasks = await Task.find({
+        assignedTo:userId,
+        organisation:req.user?.organisation
+    }).select("title description priority tags status").populate("assignedTo","name avatar")
+
+    const inProgress = tasks.filter((task)=>task.status==="in-progress");
+    const todo = tasks.filter((task)=>task.status==="todo");
+    const done = tasks.filter((task)=>task.status==="done");
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(200,{todo,inProgress,done},"Formatted tasks Fetched successfully")
+    )
+})
+
+export {
+    getUserTasks,
+}
--- server/src/routes/member.routes.js
+++ server/src/routes/member.routes.js
@@ -2,11 +2,13 @@ import { Router } from "express";
 import { verifyUser } from "../middlewares/verifyJWT.js"
 import { isMember } from "../middlewares/isMember.js";
 import { getDashboardStats, getTodaysTasks, logTaskTime } from "../controllers/member/dashboard.member.js";
+import { getUserTasks } from "../controllers/member/Taskboard.member.js";
 
 const router = Router();
 
 router.route('/dashboard-stats').get(verifyUser,isMember,getDashboardStats);
 router.route('/todays-tasks').get(verifyUser,isMember,getTodaysTasks);
 router.route('/log-time').put(verifyUser,isMember,logTaskTime);
+router.route('/tasks').get(verifyUser,isMember,getUserTasks);
 
 export default router;
