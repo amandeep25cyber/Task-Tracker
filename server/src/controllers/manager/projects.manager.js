35de8f273dc2938cf76a85c85b0e742ffe61505b
amandeep25 <st.amandeep25@gmail.com>
2026-08-28 02:16:01 +0530

    Created a logic for manager projects with aggregate pipeline with optimization of api

---
+++ server/src/controllers/manager/projects.manager.js
@@ -0,0 +1,81 @@
+import { asyncHandler } from "../../utils/AsyncHandler.js"
+import { Project } from "../../models/project.models.js"
+import { Task } from "../../models/task.models.js"
+import { ApiResponse } from "../../utils/ApiResponse.js"
+
+const managerProjects = asyncHandler(async(req,res)=>{
+    
+    const userId = req.user._id;
+    const orgId = req.user.organisation;
+
+    const projects = await Project.find({
+        organisation: orgId,
+        $or:[
+            { createdBy: userId },
+            { members: userId }
+        ]
+    })
+    .select("title description status deadline members taskCount priority")
+    .populate("members","name avatar")
+    .lean();
+
+    if (!projects.length) {
+        return res.status(200).json([]);
+    }
+
+    const projectIds = projects.map(project => project._id);
+
+    const completedTasksData = await Task.aggregate([
+        {
+            $match: {
+                project: { $in: projectIds },
+                organisation: orgId,
+                status: "done"
+            }
+        },
+        {
+            $group: {
+                _id: "$project",
+                completedCount: { $sum: 1 }
+            }
+        }
+    ]);
+
+    // Create a dictionary : { "projectId": count }
+    const completedTasksMap = completedTasksData.reduce((acc, curr) => {
+        acc[curr._id.toString()] = curr.completedCount;
+        return acc;
+    }, {});
+
+    const updatedProjects = projects.map(project => {
+        
+        const completedTask = completedTasksMap[project._id.toString()] || 0;
+        
+        const pendingTask = Math.max(0, project.taskCount - completedTask);
+        
+        const progress = project.taskCount > 0 
+            ? Math.round((completedTask / project.taskCount) * 100) 
+            : 0;
+
+        return {
+            ...project,
+            progress,
+            tasks: { 
+                total: project.taskCount,
+                completed: completedTask, 
+                pending: pendingTask 
+            }
+        };
+    });
+
+    res
+    .status(200)
+    .json(
+        new ApiResponse( 200, updatedProjects, "fetched successfully")
+    )
+});
+
+export {
+    managerProjects,
+}
+
--- server/src/routes/manager.routes.js
+++ server/src/routes/manager.routes.js
@@ -2,11 +2,13 @@ import { Router } from "express";
 import { verifyUser } from "../middlewares/verifyJWT.js";
 import { isManager } from "../middlewares/isManager.js";
 import { dashboardActiveProjects, dashboardStats, dashboardUpcomingDeadlines } from "../controllers/manager/dashboard.manager.js";
+import { managerProjects } from "../controllers/manager/projects.manager.js";
 
 const router = Router();
 
 router.route('/dashboard-stats').get(verifyUser,isManager,dashboardStats);
 router.route('/active-projects').get(verifyUser,isManager,dashboardActiveProjects);
 router.route('/upcoming-deadlines').get(verifyUser,isManager,dashboardUpcomingDeadlines);
+router.route('/projects').get(verifyUser,isManager,managerProjects);
 
 export default router;
